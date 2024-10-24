13094750820bd2ff5dc81b27f12aaf485ff1c1a6
Belhadj Med <mohamedaminbelhadj20@gmail.com>
2024-10-24 19:31:11 +0100

    trying..

--- LearnEra/src/components/header/PhMenu.jsx
+++ LearnEra/src/components/header/PhMenu.jsx
@@ -27,6 +27,7 @@ function PhMenu () {
     return (
         <Box
             sx={{
+                position:'absolute',
                 height:'100%',
                 width:'100%',
                 display:'flex',
