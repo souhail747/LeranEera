213a600dd521398314c35e3f8a6ce18d1dfdd701
Belhadj <mohamedaminbelhadj20@gmail.com>
2024-10-28 16:51:23 +0100

    nothing to commit

--- LearnEra/src/App.jsx
+++ LearnEra/src/App.jsx
@@ -1,21 +1,14 @@
 import SwiperComponent from './components/main/SwiperComponent'
 import './App.css'
-<<<<<<< HEAD
-import Header from './components/header/Header'
-import Card from './components/main/card'
-=======
+import Card from './components/main/Card'
 //import Header from './components/header/Header'
->>>>>>> 3881999f6e09354e6a5a914ecd99952ca17b7495
+
 
 function App() {
-
   return (
     <>
-<<<<<<< HEAD
     <Card />
-=======
-  <SwiperComponent/>
->>>>>>> 3881999f6e09354e6a5a914ecd99952ca17b7495
+    <SwiperComponent/>
     </>
   )
 }
